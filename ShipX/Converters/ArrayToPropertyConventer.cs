54d918ec39f6eef6bc3d4d7bd698180d1675a680
Tomasz Chabko <lordavator@gmail.com>
2018-08-01 08:57:13 +0200

    before dev branch

--- ShipX/Converters/ArrayToPropertyConventer.cs
+++ ShipX/Converters/ArrayToPropertyConventer.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,17 +31,36 @@ namespace ShipX.Net.Converters
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
 
+            JToken t = JToken.FromObject(value);
 
-            
-            
+            if (t.Type != JTokenType.Object)
+            {
+                if (t.Type == JTokenType.Array)
+                {
+                    var arr = value as Array;
 
+                    if (arr.Length == 1)
+                    {
+                        
 
+                        //arr.Length
+                    }
 
-            
+                    if (arr.Length > 1)
+                    {
 
-
-
-
+                    }
+                }
+                
+                t.WriteTo(writer);
+            }
+            else
+            {
+                JObject o = (JObject)t;
+                IList<string> propertyNames = o.Properties().Select(p => p.Name).ToList();
+                o.AddFirst(new JProperty("Keys", new JArray(propertyNames)));
+                o.WriteTo(writer);
+            }
         }
     }
 }
